1de2cc4a1fd3c065859c157c08da6c184b9c865b
t-sirakawa <sirakawa@MBPforTS2024.local>
2024-12-18 21:41:30 +0900

    adding nonblocking

--- include/sbd/hcboson/out_of_place_func/inc_all.h
+++ include/sbd/hcboson/out_of_place_func/inc_all.h
@@ -2,7 +2,11 @@
 #define SBD_HCBOSON_OUT_OF_PLACE_FUNC_INC_ALL_H
 
 #include "sbd/hcboson/out_of_place_func/arithmetic.h"
+#ifdef SBD_USE_NONBLOCKING
+#include "sbd/hcboson/out_of_place_func/mult_nb.h"
+#else
 #include "sbd/hcboson/out_of_place_func/mult_bc.h"
+#endif
 #include "sbd/hcboson/out_of_place_func/lanczos.h"
 
 #endif
