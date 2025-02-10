52d9b5601be5a9bc260dba18dd6563adb9e4daf3
t-sirakawa <t-shirakawa@riken.jp>
2025-02-10 09:22:05 +0900

    add inc all for square

---
+++ include/sbd/chemistry/square/inc_all.h
@@ -0,0 +1,9 @@
+#ifndef SBD_CHEMISTRY_SQUARE_INC_ALL_H
+#define SBD_CHEMISTRY_SQUARE_INC_ALL_H
+
+#include "sbd/chemistry/square/helper.h"
+#include "sbd/chemistry/square/qcham.h"
+#include "sbd/chemistry/square/mult.h"
+#include "sbd/chemistry/square/davidson.h"
+
+#endif
